ec29ebfc06e29cfc293a693f502199213a7d2290
Jernej Kavka (JK) [SSW] <jernejk@users.noreply.github.com>
2020-11-03 00:11:19 +1000

    Added initial implementiation of getting data from Azure DevOps (#2)
    * Added initial implementiation of getting data from Azure DevOps.
    
    * Refactored as per PR feedback.

---
+++ src/AutoScrum.AzureDevOps/Config/AzureDevOpsConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AutoScrum.AzureDevOps.Config
+{
+    public class AzureDevOpsConfig
+    {
+        public string Organization { get; set; }
+        public Uri OrganizationUrl { get; set; }
+        public string Project { get; set; }
+        public string UserEmail { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ src/AutoScrum.AzureDevOps/Models/WorkItem.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoScrum.AzureDevOps.Models
+{
+    public class WorkItem
+    {
+        public int? Id { get; set; }
+        public string? IterationPath { get; set; }
+        public string? Type { get; set; }
+        public string? State { get; set; }
+        public string? Title { get; set; }
+        public string? StateChangeDateString { get; set; }
+        public string Url { get; set; }
+    }
+}
