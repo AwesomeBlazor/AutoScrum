962d6e98aa1ea13c0ab824df170daf410081f981
Jernej Kavka (JK) <JernejKavka@ssw.com.au>
2021-07-25 13:03:29 +1000

    ✨ Support for adding/removing today/yesterday work items

---
+++ src/AutoScrum.AzureDevOps/Models/AzureDevOpsWiqlResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoScrum.AzureDevOps.Models
+{
+    public class AzureDevOpsWiqlResult
+    {
+        public string QueryType { get; set; } = string.Empty;
+        public string QueryResultType { get; set; } = string.Empty;
+        public DateTimeOffset AsOf { get; set; }
+        public List<WorkItemLink> WorkItems { get; set; } = new List<WorkItemLink>();
+    }
+}
