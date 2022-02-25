e7bc80f97c98a8bade488a59f28000c288af12ec
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2022-02-25 11:29:41 +1000

    ♻️ Refactored namespaces

--- src/AutoScrum.Tests/Services/DateServiceTests.cs
+++ src/AutoScrum.Tests/Services/DateServiceTests.cs
@@ -3,75 +3,73 @@ using System.Collections.Generic;
 using AutoScrum.Services;
 using AutoScrum.Tests.Utils;
 using FluentAssertions;
-using System.Globalization;
 using Xunit;
 
-namespace AutoScrum.Tests.Services
+namespace AutoScrum.Tests.Services;
+
+public class DateServiceTests : TestBase
 {
-    public class DateServiceTests : TestBase
+    private readonly DateService _dateService = new();
+    
+    [Theory]
+    [MemberData(nameof(SimpleDays))]
+    public void ShouldReturnYesterday(DateOnly today, DateOnly yesterday)
     {
-        private readonly DateService _dateService = new();
-        
-        [Theory]
-        [MemberData(nameof(SimpleDays))]
-        public void ShouldReturnYesterday(DateOnly today, DateOnly yesterday)
-        {
-            _dateService.GetPreviousWorkDay(today).Should().Be(yesterday);
-        }
-
-        [Theory]
-        [MemberData(nameof(WeekendAndMonday))]
-        public void ShouldReturnFriday(DateOnly today, DateOnly yesterday)
-        {
-            _dateService.GetPreviousWorkDay(today).Should().Be(yesterday);
-        }
-
-        [Theory(Skip = "Need to figure out how to test this in different timezones (changing timezone globally doesn't seem to work)")]
-        [MemberData(nameof(SimpleDayMidnights))]
-        public void ShouldReturnYesterdayMidnight(DateOnly today, DateTime yesterday)
-        {
-            _dateService.GetPreviousWorkDate(today).Should().Be(yesterday);
-        }
-
-        [Theory(Skip = "Need to figure out how to test this in different timezones (changing timezone globally doesn't seem to work)")]
-        [MemberData(nameof(WeekendAndMondayMidnight))]
-        public void ShouldReturnFridayMidnight(DateOnly today, DateTime yesterday)
-        {
-            _dateService.GetPreviousWorkDate(today).Should().BeSameDateAs(yesterday);
-        }
-
-        public static List<object[]> SimpleDays { get; set; } = new()
-        {
-            // Tuesday - Friday
-            new object[] { DateOnly.Parse("2021-07-20"), DateOnly.Parse("2021-07-19") },
-            new object[] { DateOnly.Parse("2021-07-21"), DateOnly.Parse("2021-07-20") },
-            new object[] { DateOnly.Parse("2021-07-22"), DateOnly.Parse("2021-07-21") },
-            new object[] { DateOnly.Parse("2021-07-23"), DateOnly.Parse("2021-07-22") }
-        };
-
-        public static List<object[]> WeekendAndMonday { get; set; } = new()
-        {
-            // Saturday - Monday
-            new object[] { DateOnly.Parse("2021-07-24"), DateOnly.Parse("2021-07-23") },
-            new object[] { DateOnly.Parse("2021-07-25"), DateOnly.Parse("2021-07-23") },
-            new object[] { DateOnly.Parse("2021-07-26"), DateOnly.Parse("2021-07-23") }
-        };
-
-        public static List<object[]> SimpleDayMidnights { get; set; } = new()
-        {
-            // Tuesday - Friday
-            new object[] { DateOnly.Parse("2021-07-20"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 19)) },
-            new object[] { DateOnly.Parse("2021-07-21"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 20)) },
-            new object[] { DateOnly.Parse("2021-07-22"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 21)) },
-            new object[] { DateOnly.Parse("2021-07-23"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 22)) }
-        };
-
-        public static List<object[]> WeekendAndMondayMidnight { get; set; } = new()
-        {
-            // Saturday - Monday
-            new object[] { DateOnly.Parse("2021-07-24"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 23)) },
-            new object[] { DateOnly.Parse("2021-07-25"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 23)) },
-            new object[] { DateOnly.Parse("2021-07-26"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 23)) }
-        };
+        _dateService.GetPreviousWorkDay(today).Should().Be(yesterday);
     }
+
+    [Theory]
+    [MemberData(nameof(WeekendAndMonday))]
+    public void ShouldReturnFriday(DateOnly today, DateOnly yesterday)
+    {
+        _dateService.GetPreviousWorkDay(today).Should().Be(yesterday);
+    }
+
+    [Theory(Skip = "Need to figure out how to test this in different timezones (changing timezone globally doesn't seem to work)")]
+    [MemberData(nameof(SimpleDayMidnights))]
+    public void ShouldReturnYesterdayMidnight(DateOnly today, DateTime yesterday)
+    {
+        _dateService.GetPreviousWorkDate(today).Should().Be(yesterday);
+    }
+
+    [Theory(Skip = "Need to figure out how to test this in different timezones (changing timezone globally doesn't seem to work)")]
+    [MemberData(nameof(WeekendAndMondayMidnight))]
+    public void ShouldReturnFridayMidnight(DateOnly today, DateTime yesterday)
+    {
+        _dateService.GetPreviousWorkDate(today).Should().BeSameDateAs(yesterday);
+    }
+
+    public static List<object[]> SimpleDays { get; set; } = new()
+    {
+        // Tuesday - Friday
+        new object[] { DateOnly.Parse("2021-07-20"), DateOnly.Parse("2021-07-19") },
+        new object[] { DateOnly.Parse("2021-07-21"), DateOnly.Parse("2021-07-20") },
+        new object[] { DateOnly.Parse("2021-07-22"), DateOnly.Parse("2021-07-21") },
+        new object[] { DateOnly.Parse("2021-07-23"), DateOnly.Parse("2021-07-22") }
+    };
+
+    public static List<object[]> WeekendAndMonday { get; set; } = new()
+    {
+        // Saturday - Monday
+        new object[] { DateOnly.Parse("2021-07-24"), DateOnly.Parse("2021-07-23") },
+        new object[] { DateOnly.Parse("2021-07-25"), DateOnly.Parse("2021-07-23") },
+        new object[] { DateOnly.Parse("2021-07-26"), DateOnly.Parse("2021-07-23") }
+    };
+
+    public static List<object[]> SimpleDayMidnights { get; set; } = new()
+    {
+        // Tuesday - Friday
+        new object[] { DateOnly.Parse("2021-07-20"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 19)) },
+        new object[] { DateOnly.Parse("2021-07-21"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 20)) },
+        new object[] { DateOnly.Parse("2021-07-22"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 21)) },
+        new object[] { DateOnly.Parse("2021-07-23"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 22)) }
+    };
+
+    public static List<object[]> WeekendAndMondayMidnight { get; set; } = new()
+    {
+        // Saturday - Monday
+        new object[] { DateOnly.Parse("2021-07-24"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 23)) },
+        new object[] { DateOnly.Parse("2021-07-25"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 23)) },
+        new object[] { DateOnly.Parse("2021-07-26"), TimeZoneInfo.ConvertTimeToUtc(new DateTime(2021, 07, 23)) }
+    };
 }
--- src/AutoScrum.Tests/Utils/FakeCultureInfo.cs
+++ src/AutoScrum.Tests/Utils/FakeCultureInfo.cs
@@ -1,41 +1,40 @@
 ﻿using System;
 using System.Globalization;
 
-namespace AutoScrum.Tests.Utils
+namespace AutoScrum.Tests.Utils;
+
+public class FakeCultureInfo : IDisposable
 {
-    public class FakeCultureInfo : IDisposable
+    private CultureInfo _currentCulture;
+    private CultureInfo _currentUICulture;
+
+    private CultureInfo _defaultThreadCurrentCulture;
+    private CultureInfo _defaultThreadCurrentUICulture;
+
+    public static IDisposable SetCulture(CultureInfo overrideCulture)
     {
-        private CultureInfo _currentCulture;
-        private CultureInfo _currentUICulture;
+        return new FakeCultureInfo(CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture, CultureInfo.DefaultThreadCurrentCulture, CultureInfo.DefaultThreadCurrentUICulture, overrideCulture);
+    }
 
-        private CultureInfo _defaultThreadCurrentCulture;
-        private CultureInfo _defaultThreadCurrentUICulture;
+    public FakeCultureInfo(CultureInfo currentCulture, CultureInfo currentUICulture, CultureInfo defaultThreadCurrentCulture, CultureInfo defaultThreadCurrentUICulture, CultureInfo overrideCulture)
+    {
+        _currentCulture = currentCulture;
+        _currentUICulture = currentUICulture;
+        _defaultThreadCurrentCulture = defaultThreadCurrentCulture;
+        _defaultThreadCurrentUICulture = defaultThreadCurrentUICulture;
 
-        public static IDisposable SetCulture(CultureInfo overrideCulture)
-        {
-            return new FakeCultureInfo(CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture, CultureInfo.DefaultThreadCurrentCulture, CultureInfo.DefaultThreadCurrentUICulture, overrideCulture);
-        }
+        // TODO: This doesn't seem to be working...
+        CultureInfo.CurrentCulture = overrideCulture;
+        CultureInfo.CurrentUICulture = overrideCulture;
+        CultureInfo.DefaultThreadCurrentCulture = overrideCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = overrideCulture;
+    }
 
-        public FakeCultureInfo(CultureInfo currentCulture, CultureInfo currentUICulture, CultureInfo defaultThreadCurrentCulture, CultureInfo defaultThreadCurrentUICulture, CultureInfo overrideCulture)
-        {
-            _currentCulture = currentCulture;
-            _currentUICulture = currentUICulture;
-            _defaultThreadCurrentCulture = defaultThreadCurrentCulture;
-            _defaultThreadCurrentUICulture = defaultThreadCurrentUICulture;
-
-            // TODO: This doesn't seem to be working...
-            CultureInfo.CurrentCulture = overrideCulture;
-            CultureInfo.CurrentUICulture = overrideCulture;
-            CultureInfo.DefaultThreadCurrentCulture = overrideCulture;
-            CultureInfo.DefaultThreadCurrentUICulture = overrideCulture;
-        }
-
-        public void Dispose()
-        {
-            CultureInfo.CurrentCulture = _currentCulture;
-            CultureInfo.CurrentUICulture = _currentUICulture;
-            CultureInfo.DefaultThreadCurrentCulture = _defaultThreadCurrentCulture;
-            CultureInfo.DefaultThreadCurrentUICulture = _defaultThreadCurrentUICulture;
-        }
+    public void Dispose()
+    {
+        CultureInfo.CurrentCulture = _currentCulture;
+        CultureInfo.CurrentUICulture = _currentUICulture;
+        CultureInfo.DefaultThreadCurrentCulture = _defaultThreadCurrentCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = _defaultThreadCurrentUICulture;
     }
 }
