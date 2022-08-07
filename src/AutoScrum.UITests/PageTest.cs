0713ae06384f7fcb05f2acb8d981c951132e81c1
Jernej Kavka (JK) <jk@ssw.com.au>
2022-08-07 13:10:32 +1000

    🧪 Updated to install Playwright PS and update test variables.

--- src/AutoScrum.UITests/PageTest.cs
+++ src/AutoScrum.UITests/PageTest.cs
@@ -2,32 +2,41 @@
 
 public abstract class PageTest : IAsyncLifetime
 {
-#if DEBUG
-    public const string BaseUrl = "https://autoscrum.jkdev.me/";
-#else
-    public const string BaseUrl = "https://autoscrum.jkdev.me/";
-#endif
-
     public IPlaywright PlaywrightInstance { get; private set; } = null!;
     public IBrowser Browser { get; private set; } = null!;
     public IBrowserContext BrowserContext { get; private set; } = null!;
     public IPage Page { get; private set; } = null!;
 
+    public string BaseUrl { get; private set; } = "https://localhost:5001";
+
     public virtual float? SlowMo { get; protected set; }
 
     public async Task InitializeAsync()
     {
-        PlaywrightInstance = await Playwright.CreateAsync();
-        Browser = await PlaywrightInstance.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        var browserOptions = new BrowserTypeLaunchOptions
         {
-#if DEBUG
             Headless = false,
             SlowMo = SlowMo
-#else
-            Headless = true,
-            SlowMo = null
-#endif
-        });
+        };
+
+        // Add support to modify BaseUrl from outside.
+        string? isGitHubActions = Environment.GetEnvironmentVariable("IsGitHubActions");
+        string? baseUrl = Environment.GetEnvironmentVariable("BaseUrl");
+        if (isGitHubActions == "true")
+        {
+            // Assume it's in GitHub actions and we want to run it fast.
+            browserOptions.Headless = true;
+            browserOptions.SlowMo = null;
+            baseUrl ??= "https://autoscrum.jkdev.me";
+        }
+
+        if (!string.IsNullOrWhiteSpace(baseUrl))
+        {
+            BaseUrl = baseUrl;
+        }
+
+        PlaywrightInstance = await Playwright.CreateAsync();
+        Browser = await PlaywrightInstance.Chromium.LaunchAsync(browserOptions);
 
         BrowserContext = await Browser.NewContextAsync();
         Page = await BrowserContext.NewPageAsync();
